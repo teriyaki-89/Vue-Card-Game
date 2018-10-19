e9538d6acf8fc6535ae4cc26620078c0789bec96
teriyaki-89 <idoskhozhayev@gmail.com>
2018-10-19 19:36:20 +0600

    small revisions

--- src/components/Card_Game/Card.vue
+++ src/components/Card_Game/Card.vue
@@ -16,29 +16,6 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      cards: [
-        {
-          amount: 8,
-          card_suit: 3
-        },
-        {
-          0: "spade",
-          1: "clubs",
-          2: "hearts",
-          3: "diamonds"
-        },
-        {
-          0: "6",
-          1: "7",
-          2: "8",
-          3: "9",
-          4: "10",
-          5: "Jack",
-          6: "Queen",
-          7: "King",
-          8: "Ace"
-        }
-      ],
       card_arr: [],
       card_state: [],
       pairs: []
